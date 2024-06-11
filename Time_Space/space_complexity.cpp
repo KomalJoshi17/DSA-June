ac128b1057b476018007ca891aa839f6ed8c4ee7
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-11 19:29:51 +0530

    modified:   Time_Space/space_complexity.cpp

--- Time_Space/space_complexity.cpp
+++ Time_Space/space_complexity.cpp
@@ -1,1 +1,13 @@
 // space complexity measures how the memory or space required by an algorithm grows as the size of the input increases.
+
+
+
+// observation from time analysis is that sometimes only one operation is required to push_back and add but sometimes you have to do 3*n steps(constant dosent matter)
+
+// O(1)         O(N)
+
+// if you do n(where n is the large number) push back or add operations:
+// capactity will be power of twp
+// whereever the size/n is a power of 2 then it takes 3*n steps for the next push back or add operation otherwise one step only
+
+//log base 2 n ceil value should be the highest power of 2 for capacity
