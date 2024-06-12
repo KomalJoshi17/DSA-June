f4d295fbfe857aba88adbd08ad524ae0db406542
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-12 13:52:27 +0530

    modified:   Array_2/prefix_sum.cpp

--- Array_2/prefix_sum.cpp
+++ Array_2/prefix_sum.cpp
@@ -1,28 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-class Solution {
-public:
-    vector<int> runningSum(vector<int>& nums) {
-        vector<int> ans(nums.size());
+// class Solution {
+// public:
+//     vector<int> runningSum(vector<int>& nums) {
+//         vector<int> ans(nums.size());
 
-        for(int i = 0; i < nums.size(); ++i) {
-            // nums[0] + nums[1] ... nums[i-1] + nums[i]
-            // ans[i] = ans[i-1] + nums[i]
+//         for(int i = 0; i < nums.size(); ++i) {
+//             // nums[0] + nums[1] ... nums[i-1] + nums[i]
+//             // ans[i] = ans[i-1] + nums[i]
 
-            ans[i] = i == 0? nums[i] : ans[i-1] + nums[i];
+//             ans[i] = i == 0? nums[i] : ans[i-1] + nums[i];
 
-            // if(i == 0)
-            //     ans[i] = nums[i];
-            // else
-            //     ans[i] = ans[i-1] + nums[i];
-        }
+//             // if(i == 0)
+//             //     ans[i] = nums[i];
+//             // else
+//             //     ans[i] = ans[i-1] + nums[i];
+//         }
 
-        return ans;
-    }
-};
+//         return ans;
+//     }
+// };
 
-int main() {
-	// your code goes here
-	return 0;
-}
+// int main() {
+// 	// your code goes here
+// 	return 0;
+// }
