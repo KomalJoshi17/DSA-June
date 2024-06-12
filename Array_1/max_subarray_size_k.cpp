0816754bce2d780d4d4ec12ed1bd3f4a922af0ac
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-12 12:09:25 +0530

    modified:   Array_1/max_consecutive.cpp 	modified:   Array_1/max_subarray_size_k.cpp 	new file:   Array_2/max_queries.cpp 	new file:   Array_2/merge_sorted_array.cpp 	new file:   Array_2/prefix_sum.cpp

--- Array_1/max_subarray_size_k.cpp
+++ Array_1/max_subarray_size_k.cpp
@@ -1,44 +1,79 @@
 
-class Solution {
-public:
-    long long maximumSubarraySum(vector<int>& nums, int k) {
-        if (k > nums.size()) return 0;
+// class Solution {
+// public:
+//     long long maximumSubarraySum(vector<int>& nums, int k) {
+//         if (k > nums.size()) return 0;
 
-        long long current_sum = 0;
-        long long max_sum = 0;
-        unordered_map<int, int> freq;
+//         long long current_sum = 0;
+//         long long max_sum = 0;
+//         unordered_map<int, int> freq;
 
-        // Initialize the first window
-        for (int i = 0; i < k; ++i) {
-            current_sum += nums[i];
-            freq[nums[i]]++;
-        }
+//         // Initialize the first window
+//         for (int i = 0; i < k; ++i) {
+//             current_sum += nums[i];
+//             freq[nums[i]]++;
+//         }
 
-        // Check if the initial window has all distinct elements
-        if (freq.size() == k) {
-            max_sum = current_sum;
-        }
+//         // Check if the initial window has all distinct elements
+//         if (freq.size() == k) {
+//             max_sum = current_sum;
+//         }
 
-        // Slide the window across the array
-        for (int i = k; i < nums.size(); ++i) {
-            // Remove the element that goes out of the window
-            int left_elem = nums[i - k];
-            current_sum -= left_elem;
-            if (--freq[left_elem] == 0) {
-                freq.erase(left_elem);
-            }
+//         // Slide the window across the array
+//         for (int i = k; i < nums.size(); ++i) {
+//             // Remove the element that goes out of the window
+//             int left_elem = nums[i - k];
+//             current_sum -= left_elem;
+//             if (--freq[left_elem] == 0) {
+//                 freq.erase(left_elem);
+//             }
 
-            // Add the new element coming into the window
-            int right_elem = nums[i];
-            current_sum += right_elem;
-            freq[right_elem]++;
+//             // Add the new element coming into the window
+//             int right_elem = nums[i];
+//             current_sum += right_elem;
+//             freq[right_elem]++;
 
-            // Check if the current window has all distinct elements
-            if (freq.size() == k) {
-                max_sum = max(max_sum, current_sum);
-            }
-        }
+//             // Check if the current window has all distinct elements
+//             if (freq.size() == k) {
+//                 max_sum = max(max_sum, current_sum);
+//             }
+//         }
 
-        return max_sum;
-    }
-};
+//         return max_sum;
+//     }
+// };
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long maximumSumSubarray(int K, vector<int> &arr , int N){
+	// code here
+	long cur_sum = 0;
+
+	// find the sum for 1st window of size k
+	for(int i = 0; i < K; ++i)
+		cur_sum += arr[i];
+
+	long max_sum = cur_sum;
+
+	// keep updating cur_sum as we
+	// slide through other windows
+	// and track max_sum
+	for(int st = 1, en = K; en < N; st++, en++) {
+		cur_sum -= arr[st - 1];
+		cur_sum += arr[en];
+
+		max_sum = max(max_sum, cur_sum);
+	}
+
+	return max_sum;
+}
+
+int main() {
+	// your code goes here
+	int N = 6, K = 3;
+	vector<int> arr = {5, 1, 2, 6, 3, 1};
+	cout << maximumSumSubarray(K, arr, N);
+	return 0;
+}
---
+++ Array_2/max_queries.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Time: O(N + M)
+
+void answerQueries(int arr[], int queries[], int N, int M) {
+    int *pre = new int[N];
+    int *suf = new int[N];
+
+    // Fill up the pre array
+    for(int i = 0; i < N; ++i)
+        pre[i] = i == 0? arr[i] : max(pre[i-1], arr[i]);
+
+    // Fill up the suf array
+    for(int i = N - 1; i >= 0; --i)
+        suf[i] = i == N - 1? arr[i] : max(suf[i+1], arr[i]);
+
+    // Answer the queries
+    for(int i = 0; i < M; ++i) {
+        int ans, id = queries[i];
+
+        if(id == 0)
+            ans = suf[id+1];
+        else if(id == N - 1)
+            ans = pre[id-1];
+        else
+            ans = max(pre[id-1], suf[id+1]);
+
+        cout << ans << '\n';
+    }
+}
+
+int main() {
+    int N, M; cin >> N >> M;
+    int *arr = new int[N];
+    int *queries = new int[M];
+
+    for(int i = 0; i < N; ++i)
+        cin >> arr[i];
+
+    for(int i = 0; i < M; ++i)
+        cin >> queries[i];
+
+    answerQueries(arr, queries, N, M);
+	return 0;
+}
