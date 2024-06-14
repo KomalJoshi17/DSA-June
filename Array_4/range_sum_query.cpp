d2d5635d31e4e6afbb07699df628b48fb46fb131
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-14 19:39:42 +0530

    modified:   Array_4/range_sum_query.cpp

--- Array_4/range_sum_query.cpp
+++ Array_4/range_sum_query.cpp
@@ -1,16 +1,16 @@
-class NumArray {
-public:
-    int *prefix_sum;
-    NumArray(vector<int>& nums) {
-        prefix_sum=new int[nums.size()];
-        prefix_sum[0]=nums[0];
-        for(int i=1;i<nums.size();i++){
-            prefix_sum[i]=nums[i]+prefix_sum[i-1];
-        }
-    }
-    int sumRange(int left, int right) {
-        if(left==0)
-            return prefix_sum[right];
-        return prefix_sum[right]-prefix_sum[left-1];
-    }
-};
+// class NumArray {
+// public:
+//     int *prefix_sum;
+//     NumArray(vector<int>& nums) {
+//         prefix_sum=new int[nums.size()];
+//         prefix_sum[0]=nums[0];
+//         for(int i=1;i<nums.size();i++){
+//             prefix_sum[i]=nums[i]+prefix_sum[i-1];
+//         }
+//     }
+//     int sumRange(int left, int right) {
+//         if(left==0)
+//             return prefix_sum[right];
+//         return prefix_sum[right]-prefix_sum[left-1];
+//     }
+// };
