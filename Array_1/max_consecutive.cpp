03ae87cd48d51d07e51ff81215abbe50c119c312
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-16 15:36:21 +0530

    modified:   Array_1/max_consecutive.cpp

--- Array_1/max_consecutive.cpp
+++ Array_1/max_consecutive.cpp
@@ -16,27 +16,27 @@
 // };
 
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int findMaxConsecutiveOnes(vector<int>& arr) {
-	// final_ans: stores the maximum consecutive ones.
-	// cur_count: stores the number of consecutive ones in the current window.
-	int final_ans = 0, cur_count = 0;
+// int findMaxConsecutiveOnes(vector<int>& arr) {
+// 	// final_ans: stores the maximum consecutive ones.
+// 	// cur_count: stores the number of consecutive ones in the current window.
+// 	int final_ans = 0, cur_count = 0;
 	
-	for(int num : arr) {
-		if(num == 1)
-			cur_count++;
-		else
-			cur_count = 0;
-		final_ans = max(final_ans, cur_count);
-	}
-	return final_ans;
-}
+// 	for(int num : arr) {
+// 		if(num == 1)
+// 			cur_count++;
+// 		else
+// 			cur_count = 0;
+// 		final_ans = max(final_ans, cur_count);
+// 	}
+// 	return final_ans;
+// }
 
-int main()
-{
-    vector<int> arr = {1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1};
-    cout << findMaxConsecutiveOnes(arr) << '\n';
-    return 0;
-}
+// int main()
+// {
+//     vector<int> arr = {1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1};
+//     cout << findMaxConsecutiveOnes(arr) << '\n';
+//     return 0;
+// }
