6467ae3c258fbbbe5befa21f86ec43e2c01a2f2d
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-17 11:50:03 +0530

    new file:   Array_5/key_pair.cpp

---
+++ Array_5/key_pair.cpp
@@ -0,0 +1,15 @@
+class Solution{
+public:
+	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+	    int my_memory[100001]={0};
+	    for(int i=0;i<n;i++){
+	        int curNum=arr[i];
+	        int req_num=x-curNum;
+	        if(req_num<=10000 && req_num>0 && my_memory[req_num]==1){
+                return true;
+            }
+            my_memory[curNum]=1;
+      }
+	    return false;
+	}
+};
