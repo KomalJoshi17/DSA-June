9756bcacb207cd7d5895e0171f66a1d98e40af0d
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-18 18:57:56 +0530

    new file:   Recursion/fibonacci.cpp 	modified:   Recursion/numbers_from_1_to_n.cpp 	new file:   Recursion/numbers_from_1_to_n.exe

--- Recursion/numbers_from_1_to_n.cpp
+++ Recursion/numbers_from_1_to_n.cpp
@@ -1,8 +1,17 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-    
+// last called first out
+void printN(int n){
+    if(n==0) return;
+    cout<<n<<endl;
+    printN(n-1);
+};
+
 int main(){
-    
+    int n;
+    cin>>n;
+  
+    printN(n);
     return 0;
 }
