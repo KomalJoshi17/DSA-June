d16f6386203cc2ed81f22435a1544545e5356924
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-20 19:13:36 +0530

    modified:   Array_6/subarray.cpp

--- Array_6/subarray.cpp
+++ Array_6/subarray.cpp
@@ -1,17 +1,17 @@
-class Solution {
-public:
-    int findUnsortedSubarray(vector<int>& nums) {
-        vector<int> sorted_nums = nums;
-        sort(sorted_nums.begin(),sorted_nums.end());
-        int start=-1;
-        int end=-2;
+// class Solution {
+// public:
+//     int findUnsortedSubarray(vector<int>& nums) {
+//         vector<int> sorted_nums = nums;
+//         sort(sorted_nums.begin(),sorted_nums.end());
+//         int start=-1;
+//         int end=-2;
         
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]!=sorted_nums[i]){
-                if(start==-1) start=i;
-                else end=i;
-            }
-        }
-        return end-start+1;
-    }
-};
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]!=sorted_nums[i]){
+//                 if(start==-1) start=i;
+//                 else end=i;
+//             }
+//         }
+//         return end-start+1;
+//     }
+// };
