a5c3e15974b9b7e558268df82559f31d567e74d6
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-21 10:54:37 +0530

    modified:   Recursion_Complexities/target_in_array.cpp

--- Recursion_Complexities/target_in_array.cpp
+++ Recursion_Complexities/target_in_array.cpp
@@ -1,25 +1,25 @@
-class Solution {
-    int searchHelper(vector<int> &arr, int st, int en, int k) {
-        if(st > en)
-            return -1;
+// class Solution {
+//     int searchHelper(vector<int> &arr, int st, int en, int k) {
+//         if(st > en)
+//             return -1;
             
-        if(st == en)
-            return arr[st] == k? st : -1;
+//         if(st == en)
+//             return arr[st] == k? st : -1;
         
-        int mid = (st + en)/2;    
-        int firstHalf = searchHelper(arr, st, mid, k);
+//         int mid = (st + en)/2;    
+//         int firstHalf = searchHelper(arr, st, mid, k);
         
-        if(firstHalf != -1)
-            return firstHalf;
+//         if(firstHalf != -1)
+//             return firstHalf;
             
-        int secondHalf = searchHelper(arr, mid + 1, en, k);
+//         int secondHalf = searchHelper(arr, mid + 1, en, k);
         
-        return secondHalf;
-    }
+//         return secondHalf;
+//     }
     
-    public:
-    int search(int n, int k, vector<int> &arr) {
-        int ans = searchHelper(arr, 0, n - 1, k);
-        return ans == -1? ans : ans + 1; // convert 0-based to 1-based.
-    }
-};
+//     public:
+//     int search(int n, int k, vector<int> &arr) {
+//         int ans = searchHelper(arr, 0, n - 1, k);
+//         return ans == -1? ans : ans + 1; // convert 0-based to 1-based.
+//     }
+// };
