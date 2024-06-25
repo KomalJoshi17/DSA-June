ecf24070393a4dbf63cc3803d1cdfe5099949505
Komal Joshi <moudgillkomal6@gmail.com>
2024-06-25 19:08:06 +0530

    new file:   .vscode/settings.json 	modified:   Sorting/SORTING.cpp 	modified:   Sorting/SORTING.exe

--- Sorting/SORTING.cpp
+++ Sorting/SORTING.cpp
@@ -16,6 +16,32 @@ void bubbleSort(int arr[],int n){
         }
     }
 };
+
+void selectionSort(int arr[],int n){
+    for(int i=0;i<n;i++){
+        int min_index=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[min_index]){
+                min_index=j;
+            }
+        }
+        swap(arr[min_index],arr[i]);
+    }
+};
+
+void insertionSort(int arr[],int n){
+    
+    for(int i=1;i<n;i++){
+        int key = arr[i];
+        int j=i-1;
+        while(j>=0 && arr[j]>key){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+};
+
 int main(){
     int n;
     cin>>n;
@@ -26,7 +52,10 @@ int main(){
     }
 
     bubbleSort(arr,n);
-
+    cout<<endl;
+    selectionSort(arr,n);
+    cout<<endl;
+    
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
