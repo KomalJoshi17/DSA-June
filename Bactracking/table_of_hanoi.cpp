7e69866a31da10f12387adf356654541308812f8
Komal Joshi <moudgillkomal6@gmail.com>
2024-07-30 08:20:58 +0530

    modified:   Bactracking/table_of_hanoi.cpp

--- Bactracking/table_of_hanoi.cpp
+++ Bactracking/table_of_hanoi.cpp
@@ -1,15 +1,15 @@
-// Link : https://www.geeksforgeeks.org/problems/tower-of-hanoi/1
+// // Link : https://www.geeksforgeeks.org/problems/tower-of-hanoi/1
 
-import java.io.*;
-import java.util.*;
-class Recursion {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+// import java.io.*;
+// import java.util.*;
+// class Recursion {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();//total testcases
-        while (T-- > 0) {
-…        // the help of 'from'.
-        moves += toh(N - 1, aux, to, from);
-        return moves;
-    }
-}
+//         int T = sc.nextInt();//total testcases
+//         while (T-- > 0) {
+// …        // the help of 'from'.
+//         moves += toh(N - 1, aux, to, from);
+//         return moves;
+//     }
+// }
